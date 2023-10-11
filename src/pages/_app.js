2a3df2b46429af55beaee042227da42a03107b8d
Manav Arora <manavarora506@utexas.edu>
2023-10-10 21:42:10 -0500

    trying

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,21 @@
+// import "../styles/custom-styles.css";
+// import "../styles/dracula.css";
+// import "../styles/prose-styles.css";
+
+// import * as React from "react";
+// import { Nav } from "../components/Nav";
+// import Chatbot from "../components/Chatbot";
+
+// export default function App({ Component, pageProps }) {
+//   return (
+//     <>
+//       <Nav />
+//       <Component {...pageProps} />
+//       <Chatbot />
+//     </>
+//   );
+// }
+
 import "../styles/custom-styles.css";
 import "../styles/dracula.css";
 import "../styles/prose-styles.css";
@@ -9,8 +27,10 @@ import Chatbot from "../components/Chatbot";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Nav />
-      <Component {...pageProps} />
+      <div className="site-wrapper" style={{ overflowX: 'hidden', position: 'relative' }}>
+        <Nav />
+        <Component {...pageProps} />
+      </div>
       <Chatbot />
     </>
   );
