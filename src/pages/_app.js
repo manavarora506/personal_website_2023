6bb34aee75093758e38a407d15be849f070d13c0
Manav Arora <manavarora506@utexas.edu>
2023-11-19 01:37:54 -0600

    adding google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import "../styles/prose-styles.css";
 import * as React from "react";
 import { Nav } from "../components/Nav";
 import Chatbot from "../components/Chatbot";
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -12,6 +13,24 @@ export default function App({ Component, pageProps }) {
       <Nav />
       <Component {...pageProps} />
       <Chatbot />
+
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-QP9R4B7G1K"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-QP9R4B7G1K');
+        `}
+      </Script>
     </>
   );
 }
